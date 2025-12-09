5ed2882eba756d23c704803d84b7b916276ed679
samris1626 <samris1626@students.ecpi.edu>
2025-12-08 23:25:10 -0500

    Update Claimant class documentation and formatting

--- Claimant.cs
+++ Claimant.cs
@@ -1,42 +1,42 @@
 /************************************************************
 * Name: Samantha Riser
-* Date: 11/30/2025
-* Assignment: SDC320 Course Project - Week 3
-* Represents a claimant who is filing a lawsuit. 
-* Stores personal contact information and the business associated with the claimant.
-* This class is part of the core data model and will map directly to the Claimants table in the database.
+* Date: 12/8/2025
+* Assignment: SDC320 Course Project - Week 4
 *
+* Represents a claimant filing a lawsuit.
+* Maps to Claimants table in SQLite database.
 */
-public class Claimant
+
+namespace LegalCaseManagement
 {
-    // Unique identifier for the claimant (Primary Key in database)
-    public int ClaimantID { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Email { get; set; }
-    public int BusinessID { get; set; }
-
-    // Default constructor (required for object initialization)
-    public Claimant() { }
-
-    // Constructor that allows quick creation of a claimant object
-    public Claimant(int claimantID, string firstName, string lastName, string phoneNumber, string email, int businessID)
+    public class Claimant
     {
-        ClaimantID = claimantID;
-        FirstName = firstName;
-        LastName = lastName;
-        PhoneNumber = phoneNumber;
-        Email = email;
-        BusinessID = businessID;
-    }
+        public int ClaimantID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int BusinessID { get; set; }
 
-    public override string ToString()
-    {
-        return $"Claimant ID: {ClaimantID}\n" +
-               $"Name: {FirstName} {LastName}\n" +
-               $"Phone: {PhoneNumber}\n" +
-               $"Email: {Email}\n" +
-               $"Business ID: {BusinessID}";
+        public Claimant() { }
+
+        public Claimant(int claimantID, string firstName, string lastName, string phoneNumber, string email, int businessID)
+        {
+            ClaimantID = claimantID;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            BusinessID = businessID;
+        }
+
+        public override string ToString()
+        {
+            return $"Claimant ID: {ClaimantID}\n" +
+                   $"Name: {FirstName} {LastName}\n" +
+                   $"Phone: {PhoneNumber}\n" +
+                   $"Email: {Email}\n" +
+                   $"Business ID: {BusinessID}";
+        }
     }
 }
