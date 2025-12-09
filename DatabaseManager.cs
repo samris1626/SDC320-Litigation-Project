f4d445339110fcbed8bf4b7cf8a957c2587d18e7
samris1626 <samris1626@students.ecpi.edu>
2025-12-08 23:17:57 -0500

    Implement CRUD operations for Business and Case

--- DatabaseManager.cs
+++ DatabaseManager.cs
@@ -143,7 +143,7 @@ namespace LegalCaseManagement
                 cmd.ExecuteNonQuery();
             }
         }
-        
+
         // BUSINESS CRUD
         public void AddBusiness(Business b)
         {
@@ -152,4 +152,158 @@ namespace LegalCaseManagement
                 conn.Open();
                 string sql = @"INSERT INTO Businesses 
                                (BusinessName, ContactPhone, Address)
-                               VALUES
+                               VALUES (@n, @p, @a);";
+
+                var cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@n", b.BusinessName);
+                cmd.Parameters.AddWithValue("@p", b.ContactPhone);
+                cmd.Parameters.AddWithValue("@a", b.Address);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public Business GetBusiness(int id)
+        {
+            using (var conn = new SQLiteConnection(ConnectionString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM Businesses WHERE BusinessID=@id";
+
+                var cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new Business
+                        {
+                            BusinessID = Convert.ToInt32(reader["BusinessID"]),
+                            BusinessName = reader["BusinessName"].ToString(),
+                            ContactPhone = reader["ContactPhone"].ToString(),
+                            Address = reader["Address"].ToString()
+                        };
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void UpdateBusiness(Business b)
+        {
+            using (var conn = new SQLiteConnection(ConnectionString))
+            {
+                conn.Open();
+                string sql = @"UPDATE Businesses SET
+                               BusinessName=@n, ContactPhone=@p, Address=@a
+                               WHERE BusinessID=@id;";
+
+                var cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@n", b.BusinessName);
+                cmd.Parameters.AddWithValue("@p", b.ContactPhone);
+                cmd.Parameters.AddWithValue("@a", b.Address);
+                cmd.Parameters.AddWithValue("@id", b.BusinessID);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteBusiness(int id)
+        {
+            using (var conn = new SQLiteConnection(ConnectionString))
+            {
+                conn.Open();
+                string sql = "DELETE FROM Businesses WHERE BusinessID=@id";
+
+                var cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        // CASE CRUD
+        public void AddCase(CaseRecord c)
+        {
+            using (var conn = new SQLiteConnection(ConnectionString))
+            {
+                conn.Open();
+                string sql = @"INSERT INTO Cases
+                               (CaseName, CaseDescription, ClaimantID, BusinessID)
+                               VALUES (@n, @d, @cid, @bid);";
+
+                var cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@n", c.CaseName);
+                cmd.Parameters.AddWithValue("@d", c.CaseDescription);
+                cmd.Parameters.AddWithValue("@cid", c.ClaimantID);
+                cmd.Parameters.AddWithValue("@bid", c.BusinessID);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public CaseRecord GetCase(int id)
+        {
+            using (var conn = new SQLiteConnection(ConnectionString))
+            {
+                conn.Open();
+                string sql = "SELECT * FROM Cases WHERE CaseID=@id";
+
+                var cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                using (var reader = cmd.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        return new CaseRecord
+                        {
+                            CaseID = Convert.ToInt32(reader["CaseID"]),
+                            CaseName = reader["CaseName"].ToString(),
+                            CaseDescription = reader["CaseDescription"].ToString(),
+                            ClaimantID = Convert.ToInt32(reader["ClaimantID"]),
+                            BusinessID = Convert.ToInt32(reader["BusinessID"])
+                        };
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void UpdateCase(CaseRecord c)
+        {
+            using (var conn = new SQLiteConnection(ConnectionString))
+            {
+                conn.Open();
+                string sql = @"UPDATE Cases SET
+                               CaseName=@n, CaseDescription=@d,
+                               ClaimantID=@cid, BusinessID=@bid
+                               WHERE CaseID=@id;";
+
+                var cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@n", c.CaseName);
+                cmd.Parameters.AddWithValue("@d", c.CaseDescription);
+                cmd.Parameters.AddWithValue("@cid", c.ClaimantID);
+                cmd.Parameters.AddWithValue("@bid", c.BusinessID);
+                cmd.Parameters.AddWithValue("@id", c.CaseID);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteCase(int id)
+        {
+            using (var conn = new SQLiteConnection(ConnectionString))
+            {
+                conn.Open();
+                string sql = "DELETE FROM Cases WHERE CaseID=@id";
+
+                var cmd = new SQLiteCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
